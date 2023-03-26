11156c5605c21f3d3711dbad351bf60a3f6c1ab8
TARUN GUPTA <126667434+sdfcdevops@users.noreply.github.com>
2023-03-27 03:44:21 +0530

    added jest test success

---
+++ LwcWithJestTest/force-app/main/default/lwc/helloLightingWebComponent/helloLightingWebComponent.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+
+export default class HelloLightingWebComponent extends LightningElement {
+
+    message = 'Tarun Jest Test!';
+  
+  handleChange(event) {
+    this.message = event.target.value;
+  }
+}
