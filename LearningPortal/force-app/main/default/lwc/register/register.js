8be5bbe4fc46e4dbe3a6dc6eb4cf57ddbba8ec66
TARUN GUPTA <126667434+sdfcdevops@users.noreply.github.com>
2023-04-12 16:49:24 +0530

    no message

---
+++ LearningPortal/force-app/main/default/lwc/register/register.js
@@ -0,0 +1,7 @@
+import { LightningElement } from 'lwc';
+import MY_IMAGE from '@salesforce/resourceUrl/backgroundImg';
+
+export default class Register extends LightningElement {
+    imageUrl = MY_IMAGE;
+    
+}
